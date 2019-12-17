1633e2be88891e08e1587138b8d62c55c19efd11
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-17 10:26:24 +0545

    Adding Merging sorted list function

--- src/main/kotlin/challenges/linkedList/ReverseLinkedList.kt
+++ src/main/kotlin/challenges/linkedList/ReverseLinkedList.kt
@@ -3,11 +3,12 @@ package challenges.linkedList
 import datastructures.linkedList.LinkedList
 import datastructures.linkedList.Node
 
+// Solution 2: using recursive function to reverse the lit
 // recursive extension function; calls itself on the next node
-fun <Any> Node<Any>?.printInReverse() {
+fun <T> Node<T>?.printInReverse() {
     this?.next?.printInReverse() // terminates after next = null
 
-    // any print coming after the above recursive call, is only triggered after the final/base case
+    // T print coming after the above recursive call, is only triggered after the final/base case
     // i.e when next = null => so this prints the list in reverse
     if (this?.next != null) {
         print(" -> ")
@@ -16,13 +17,102 @@ fun <Any> Node<Any>?.printInReverse() {
 }
 
 // extension function way of doing things
-fun <Any> LinkedList<Any>.printInReverse() {
+fun <T> LinkedList<T>.printInReverse() {
     this.findNodeAt(0)?.printInReverse()
 }
+// end of Solution 2
 
-class ReverseLinkedList(private val linkedList: LinkedList<Any>) {
-    fun listReverser(): LinkedList<Any> {
-        val reversedList = LinkedList<Any>()
+/**
+ * Solution 3: One solution is to have two references traverse down the nodes of the list where
+ * one is twice as fast as the other. Once the faster reference reaches the end,
+ * the slower reference will be in the middle.
+ * */
+// Time complexity: O(n) since it traverses the whole list once
+fun <T> LinkedList<T>.getMiddle(): Node<T>? {
+    var slow = this.findNodeAt(0)
+    var fast = this.findNodeAt(0)
+
+    while (fast != null) {
+        fast = fast.next
+        if (fast != null) {
+            fast = fast.next
+            slow = slow?.next
+        }
+    }
+
+    return slow
+}
+
+// Solution 4: time complexity is O(n) but also space complexity: O(n) since it creates a new list
+/**
+ * reverse a list by using a recursive function that goes to the end of the list and
+ * then starts copying the nodes when it returns, in a new linked list
+ */
+private fun <T> addInReverse(list: LinkedList<T>, node: Node<T>) {
+    // 1: go to next
+    val next = node.next
+
+    if (next != null) {
+        // 2: if we haven't reached the end, recursively call this function
+        addInReverse(list, next)
+    }
+
+    // 3: once we reached the end, start adding to the list from the end
+    list.append(node.data)
+}
+
+// extension function to test the addInReverse() above
+fun <T> LinkedList<T>.reversed(): LinkedList<T> {
+    val result = LinkedList<T>()
+
+    val head = this.findNodeAt(0)
+    if (head != null) {
+        addInReverse(result, head)
+    }
+
+    return result
+}
+
+// Solution 1: merging two sorted lists
+
+// helper function: adds the current node to the result list and returns the next node.
+private fun <T : Comparable<T>> append(
+        result: LinkedList<T>,
+        node: Node<T>
+): Node<T>? {
+    result.append(node.data)
+    return node.next
+}
+
+// time complexity: O(m + n) where m and n are number of nodes in each list
+fun <T : Comparable<T>> LinkedList<T>.mergeSorted(otherList: LinkedList<T>)
+        : LinkedList<T> {
+    // 1: deal with case when one or other list is empty
+    if (otherList.isEmpty()) return this
+    if (this.isEmpty()) return otherList
+
+    val result = LinkedList<T>()
+    // 1
+    var thisNode = this.findNodeAt(0)
+    var otherNode = otherList.findNodeAt(0)
+    // 2
+    while (thisNode != null && otherNode != null) {
+        // 3
+        if (thisNode.data < otherNode.data) thisNode = append(result, thisNode)
+        else otherNode = append(result, otherNode)
+    }
+
+    // 4: handle the remaining nodes in any of the two lists
+    while (thisNode != null) thisNode = append(result, thisNode)
+    while (otherNode != null) otherNode = append(result, otherNode)
+
+    return result
+}
+
+class ReverseLinkedList<T>(private val linkedList: LinkedList<T>) {
+    // Solution 1: using push to reverse the list
+    fun listReverser(): LinkedList<T> {
+        val reversedList = LinkedList<T>()
 
         // loop through original list and append elements to reversedList
         val iterator = linkedList.iterator()
@@ -36,8 +126,28 @@ class ReverseLinkedList(private val linkedList: LinkedList<Any>) {
     }
 }
 
+private fun testMergeSorted() {
+    val list = LinkedList<Int>()
+    list.add(1)
+    list.add(2)
+    list.add(3)
+    list.add(4)
+    list.add(5)
+
+    val other = LinkedList<Int>()
+    other.add(-1)
+    other.add(0)
+    other.add(2)
+    other.add(2)
+    other.add(7)
+
+    println("Left: $list")
+    println("Right: $other")
+    println("Merged: ${list.mergeSorted(other)}")
+}
+
 fun main() {
-    val linkedList = LinkedList<Any>()
+    val linkedList = LinkedList<Int>()
     linkedList.append(3)
     linkedList.append(2)
     linkedList.append(1)
@@ -49,4 +159,10 @@ fun main() {
 
     print("Reversed list using extension function: ")
     linkedList.printInReverse()
+
+    println("\n experimenting with print middle: ${linkedList.getMiddle()}")
+
+    println("experimenting with reversed(): ${linkedList.reversed()}")
+
+    testMergeSorted()
 }
