4f9752fbee9849510e9698857eb82ef352a7ac12
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-12-19 15:46:13 +0545

    Added Depth first and breadth/level-order traversals.

--- src/main/kotlin/datastructures/tree/TreeNode.kt
+++ src/main/kotlin/datastructures/tree/TreeNode.kt
@@ -1,14 +1,50 @@
 package datastructures.tree
 
+import java.util.*
+
 class TreeNode<T>(val value: T) {
     private val children: MutableList<TreeNode<T>> = mutableListOf()
 
     fun addChild(child: TreeNode<T>) = children.add(child)
 
+    /**
+     * Depth-first Traversal
+     */
+    fun forEachDepthFirst(visit: Visitor<T>) {
+        visit(this) // visit the root which is the this reference
+        children.forEach {
+            it.forEachDepthFirst(visit)
+        }
+
+    }
+
+    /**
+     * Level-order (or breadth first) traversal
+     */
+    fun forEachLevelOrder(visit: Visitor<T>) {
+        visit(this) // visit the root first
+        val queue: Queue<TreeNode<T>> = LinkedList<TreeNode<T>>()
+
+        children.forEach { queue.add(it) }
+
+        var node: TreeNode<T>? = queue.remove()
+        while (node != null) {
+            visit(node)
+            node.children.forEach { queue.add(it) }
+            node = if (queue.size > 0) {
+                queue.remove()
+            } else {
+                null
+            }
+        }
+    }
+
     override fun toString(): String {
         return "TreeNode(value=$value, children=$children)"
     }
 }
+
+typealias Visitor<T> = (TreeNode<T>) -> Unit
 
 // main method for quick, dirty testing only; prefer to use Unit tests
 fun main() {
